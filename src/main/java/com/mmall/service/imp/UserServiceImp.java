449d6d054934026250dc6643d3d68db554b7d488
78663 <qwer123r45t@163.com>
2017-08-01 11:04:15 +0800

    login in finish

---
+++ src/main/java/com/mmall/service/imp/UserServiceImp.java
@@ -0,0 +1,33 @@
+package com.mmall.service.imp;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.UserMapper;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service("iUserService")
+public class UserServiceImp implements IUserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public ServerResponse<User> login(String username, String password) {
+        int resultCount = userMapper.checkUserName(username);
+        if(resultCount == 0){
+            return ServerResponse.createByErrorMessage("用户名不存在");
+        }
+
+        // todo 密码登录MD5
+
+        User user = userMapper.selectLogin(username,password);
+        if(user == null){
+            return ServerResponse.createByErrorMessage("密码错误");
+        }
+        user.setPassword(org.apache.commons.lang3.StringUtils.EMPTY);
+        return ServerResponse.createBySuccess("登录成功",user);
+
+    }
+}
