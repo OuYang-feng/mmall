a903c727885fe35c1aff264253b7204ca719d5c7
78663 <qwer123r45t@163.com>
2017-08-17 10:42:57 +0800

    shipping address block finished

---
+++ src/main/java/com/mmall/service/imp/CartServiceImp.java
@@ -0,0 +1,145 @@
+package com.mmall.service.imp;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.CartMapper;
+import com.mmall.dao.ProductMapper;
+import com.mmall.pojo.Cart;
+import com.mmall.pojo.Product;
+import com.mmall.service.ICartService;
+import com.mmall.util.BigDecimalUtil;
+import com.mmall.util.PropertiesUtil;
+import com.mmall.vo.CartProductVo;
+import com.mmall.vo.CartVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.apache.commons.collections.CollectionUtils;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Spliterator;
+
+@Service("iCartService")
+public class CartServiceImp implements ICartService{
+    @Autowired
+    private CartMapper cartMapper;
+
+    @Autowired
+    private ProductMapper productMapper;
+
+
+    public ServerResponse<CartVo> add(Integer userId,Integer productId,Integer count){
+        if (productId == null || count == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.ILLEGAL_ARGUMENT.getCode(),ResponseCode.ILLEGAL_ARGUMENT.getDesc());
+        }
+
+        Cart cart = cartMapper.selectCartByUserIdProductId(userId,productId);
+        if (cart==null){
+            //如果产品不在购物车中，需要新增一个这个产品记录
+            Cart cartItem = new Cart();
+            cartItem.setQuantity(count);
+            cartItem.setChecked(Const.Cart.CHECKED);
+            cartItem.setProductId(productId);
+            cartItem.setUserId(userId);
+            cartMapper.insert(cartItem);
+        }else {
+            //产品已经在购物车中了
+            count = cart.getQuantity() + count;
+            cart.setQuantity(count);
+            cartMapper.updateByPrimaryKeySelective(cart);
+        }
+        CartVo cartVo = this.getCartVoLimit(userId);
+        return ServerResponse.createBySuccess(cartVo);
+    }
+
+    private CartVo getCartVoLimit(Integer userId){
+        CartVo cartVo = new CartVo();
+        List<Cart> cartList = cartMapper.selectCartByUserId(userId);
+        List<CartProductVo> cartProductVoList = Lists.newArrayList();
+
+        BigDecimal cartTotalPrice = new BigDecimal("0");
+
+        if (CollectionUtils.isNotEmpty(cartList)){
+            for (Cart cartItem : cartList){
+                CartProductVo cartProductVo = new CartProductVo();
+                cartProductVo.setId(cartItem.getId());
+                cartProductVo.setUserId(cartItem.getUserId());
+                cartProductVo.getProductId(cartItem.getProductId());
+
+                Product product = productMapper.selectByPrimaryKey(cartItem.getProductId());
+                if (product != null){
+                    cartProductVo.setProductMainImage(product.getMainImage());
+                    cartProductVo.setProductName(product.getName());
+                    cartProductVo.setProductSubtitle(product.getSubtitle());
+                    cartProductVo.setProductStatus(product.getStatus());
+                    cartProductVo.setProductPrice(product.getPrice());
+                    cartProductVo.setProductStock(product.getStock());
+
+                    int buyLimitCount = 0;
+                    if (product.getStock() >= cartItem.getQuantity()){
+                        //库存充足的时候
+                        buyLimitCount = cartItem.getQuantity();
+                        cartProductVo.setLimitQuantity(Const.Cart.LIMIT_NUM_SUCCESS);
+                    }else {
+                        buyLimitCount = product.getStock(); //等于产品库存
+                        cartProductVo.setLimitQuantity(Const.Cart.LIMIT_NUM_FAIL);
+                        //购物车中更新有效库存
+                        Cart cartForQuantity = new Cart();
+                        cartForQuantity.setId(cartItem.getId());
+                        cartForQuantity.setQuantity(buyLimitCount);
+                        cartMapper.updateByPrimaryKeySelective(cartForQuantity);
+                    }
+                    cartProductVo.setQuantity(buyLimitCount);
+                    //计算总价
+                    cartProductVo.setProductTotalPrice(BigDecimalUtil.mul(product.getPrice().doubleValue(),cartProductVo.getQuantity()));
+                    cartProductVo.setProductChecked(cartItem.getChecked());
+                }
+                if (cartItem.getChecked() == Const.Cart.CHECKED){
+                    //如果勾选则增加到总价中
+                    cartTotalPrice = BigDecimalUtil.add(cartTotalPrice.doubleValue(),cartProductVo.getProductTotalPrice().doubleValue());
+                }
+
+                cartProductVoList.add(cartProductVo);
+            }
+        }
+        cartVo.setCartTotalPrice(cartTotalPrice);
+        cartVo.setCartProductVoList(cartProductVoList);
+        cartVo.getAllChecked(this.getAllCheckedStatus(userId));
+        cartVo.setImageHost(PropertiesUtil.getProperty("ftp.server.http.prefix"));
+
+        return null;
+    }
+
+    private boolean getAllCheckedStatus(Integer userId){
+        if (userId == null){
+            return false;
+        }
+        return cartMapper.selectCartProductCheckedStatusByUserId(userId) == 0;
+    }
+
+    public ServerResponse<CartVo> update(Integer userId,Integer productId,Integer count){
+        if (productId == null || count == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.ILLEGAL_ARGUMENT.getCode(),ResponseCode.ILLEGAL_ARGUMENT.getDesc());
+        }
+        Cart cart = cartMapper.selectCartByUserIdProductId(userId,productId);
+        if (cart != null){
+            cart.setQuantity(count);
+        }
+        cartMapper.updateByPrimaryKeySelective(cart);
+        CartVo cartVo = this.getCartVoLimit(userId);
+        return ServerResponse.createBySuccess(cartVo);
+    }
+
+    public ServerResponse<CartVo> deleteProduct(Integer userId,String productIds){
+        List<String> productList = Splitter.on(",").splitToList(productIds);
+        if (CollectionUtils.isEmpty(productList)) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.ILLEGAL_ARGUMENT.getCode(),ResponseCode.ILLEGAL_ARGUMENT.getDesc());
+        }
+        cartMapper.deleteByUserIdProductIds(userId,productList);
+        CartVo cartVo = this.getCartVoLimit(userId);
+        return ServerResponse.createBySuccess(cartVo);
+    }
+}
